6d40d2d591b31a13bc6071cf6d8626e167e7d42e
SeifAkmal <seifakmal5@gmail.com>
2025-11-13 07:27:39 +0200

    refactor: improve code structure for better readability and maintainability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: 'products', pathMatch: 'full' },
+];
